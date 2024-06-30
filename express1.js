884cf3932319cbd3b4125346556d9f8710666d97
R00T-Kim <142708794+R00T-Kim@users.noreply.github.com>
2024-07-01 00:43:02 +0900

    Update express1.js

--- express1.js
+++ express1.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-app.get('/express', (req,res) => {
-    res.send('<h1> Hello server!!</h1>');
+app.get('/express', (req,res)=>{
+    res.send('<h1>Hello server!!</h1>')
+});
+
+app.get('/:ingoo', (req,res)=>{
+    let name= req.params.ingoo
+    res.send(`<h1>Hello ${name}</h1>`)
 });
 
 app.listen(3000, () => {
